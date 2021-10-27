72b5d4390ca46f9ae2480a124c7289c378d37ea7
unknown <moralesnmatias@gmail.com>
2021-10-27 16:10:39 -0300

    Modificaciones pagina Home (Diseño)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -73,7 +73,7 @@
                                 </div>
                                 
                                 <div class="col-md-4 col-sm-6 mb-sm-50 mb-xs-30">
-                                    Sabemos la dificil tarea que es la realización de tu primer Curriculum Vitae. Por eso nuestra mision es ayudarte de manera sencilla e intuitiva en la creacion de tu propio CV.   
+                                    Sabemos la dificil tarea que es la realización de tu primer Curriculum Vitae. Por eso nuestra mision es ayudarte de manera sencilla e intuitiva en su creación.   
                                 </div>
                                 
                                 <div class="col-md-4 col-sm-6 mb-sm-50 mb-xs-30">
@@ -82,7 +82,12 @@
                                 
                             </div>
                         </div>
-                        
+
+                        <!-- Divider -->
+                        <hr class="mt-0 mb-0 "/>
+                        <br>
+                        <!-- End Divider -->
+
                         <div class="row">
                             <h2 class="section-title font-alt align-left mb-70 mb-sm-40">
                                 Desarrolladores
@@ -238,7 +243,7 @@
                                         <span class="icon-strategy"></span>
                                     </div>
                                     
-                                    Branding
+                                    Rápido y facil
                                 </a>
                             </li>
                             <li>
@@ -247,8 +252,7 @@
                                     <div class="alt-tabs-icon">
                                         <span class="icon-desktop"></span>
                                     </div>
-                                    
-                                    Web Design
+                                    Mayor oportunidades
                                 </a>
                             </li>
                             <li>
@@ -258,29 +262,10 @@
                                         <span class="icon-tools"></span>
                                     </div>
                                     
-                                    Graphic Design
+                                    Organiza tus aplicaciones
                                 </a>
                             </li>
-                            <li>
-                                <a href="#service-development" class="nav-link" data-bs-toggle="tab" role="tab" aria-selected="false">
-                                    
-                                    <div class="alt-tabs-icon">
-                                        <span class="icon-gears"></span>
-                                    </div>
-                                    
-                                    Development
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#service-photography" class="nav-link" data-bs-toggle="tab" role="tab" aria-selected="false">
-                                    
-                                    <div class="alt-tabs-icon">
-                                        <span class="icon-camera"></span>
-                                    </div>
-                                    
-                                    Photography
-                                </a>
-                            </li>
+                            
                         </ul>
                         <!-- End Nav tabs -->
                         
@@ -295,24 +280,16 @@
                                         <div class="col-md-4 mb-md-40 mb-xs-30">
                                             <blockquote class="mb-0">
                                                 <p>
-                                                    A&nbsp;brand for a&nbsp;company is&nbsp;like a&nbsp;reputation 
-                                                    for a&nbsp;person. You earn reputation by&nbsp;trying to&nbsp;do&nbsp;hard 
-                                                    things well.
+                                                Con nuestro creador de CV en línea es fácil para todos crear rápidamente un CV profesional. 
                                                 </p>
-                                                <footer>
-                                                    Jeff Bezos
-                                                </footer>
+                                                
                                             </blockquote>
                                         </div>
                                         <div class="col-md-4 col-sm-6 mb-sm-50 mb-xs-30">
-                                            Maecenas  volutpat, diam enim sagittis quam, id porta quam. Sed id dolor consectetur fermentum 
-                                            volutpat nibh, accumsan purus. Lorem ipsum dolor sit semper amet, consectetur adipiscing elit. 
-                                            In maximus ligula metus pellentesque mattis.  
+                                        Usted ingresa sus datos personales y luego comienza a completar el contenido de su CV. 
                                         </div>
                                         <div class="col-md-4 col-sm-6 mb-sm-50 mb-xs-30">
-                                            Donec vel ultricies purus. Nam dictum sem, ipsum aliquam . Etiam sit amet fringilla lacus. 
-                                            Pelenteque suscipit ante at ullamcorper pulvinar neque portor. Integer lectus. 
-                                            Praesent sed nisi eleifend, fermetum orci amet, iaculis libero. 
+                                        Finalmente, deje que el sistema disponga del CV mas recomendable para usted y descarguelo.
                                         </div>
                                     </div>
                                 </div>
@@ -328,23 +305,16 @@
                                         <div class="col-md-4 mb-md-40 mb-xs-30">
                                             <blockquote class="mb-0">
                                                 <p>
-                                                    It&nbsp;doesn&rsquo;t matter how many times&nbsp;I have to&nbsp;click, as&nbsp;long 
-                                                    as&nbsp;each click is&nbsp;a&nbsp;mindless, unambiguous choice.
+                                                Es más probable que consiga un trabajo.
                                                 </p>
-                                                <footer>
-                                                    Steve Krug
-                                                </footer>
+                                                
                                             </blockquote>
                                         </div>
                                         <div class="col-md-4 col-sm-6 mb-sm-50 mb-xs-30">
-                                            Cras mi tortor, laoreet id ornare et, accumsan non magna. Maecenas vulputate accumsan velit. 
-                                            Curabitur a nulla ex. Nam a tincidunt ante. Vitae gravida turpis. Vestibulum varius
-                                            nulla non nulla scelerisque tristique.
+                                        Con un curriculum vitae representativo y profesional usted se destacará entre otros solicitantes. 
                                         </div>
                                         <div class="col-md-4 col-sm-6 mb-sm-50 mb-xs-30">
-                                            Mauris id viverra augue, eu porttitor diam. Praesent faucibus est a interdum elementum. 
-                                            Nam varius at ipsum id dignissim. Nam a tincidunt ante lorem. Pellentesque suscipit ante
-                                            at ullamcorper pulvinar neque porttitor. 
+                                        También tiene hasta un 65% más de probabilidades de ser invitado a una entrevista.
                                         </div>
                                     </div>
                                 </div>
@@ -360,24 +330,15 @@
                                         <div class="col-md-4 mb-md-40 mb-xs-30">
                                             <blockquote class="mb-0">
                                                 <p>
-                                                    Never fall in&nbsp;love with an&nbsp;idea. They&rsquo;re whores. If&nbsp;the one you&rsquo;re with isn&rsquo;t doing the job, there&rsquo;s always another.
+                                                A menudo, es importante poder ajustar los CV en función del trabajo que está solicitando.
                                                 </p>
-                                                <footer>
-                                                    Chip Kidd
-                                                </footer>
+                                    
                                             </blockquote>
                                         </div>
                                         <div class="col-md-4 col-sm-6 mb-sm-50 mb-xs-30">
-                                            Lorem ipsum dolor sit semper amet, consectetur adipiscing elit. In maximus ligula metus pellentesque
-                                            mattis. Donec vel ultricies purus. Nam dictum sem, ipsum aliquam . Praesent sed nisi eleifend, 
-                                            fermentum orci amet, iaculis libero. 
+                                        Con el creador de CV, puede crear y administrar varios CV de forma organizada en su propio entorno de cuenta personal.
                                         </div>
-                                        <div class="col-md-4 col-sm-6 mb-sm-50 mb-xs-30">
-                                             
-                                            Pellentesque suscipit ante at ullamcorper pulvinar neque porttitor. Integer lectus. Etiam sit amet 
-                                            fringilla lacus. Maecenas  volutpat, diam enim sagittis quam, id porta quam. Sed id&nbsp;dolor 
-                                            consectetur fermentum volutpat nibh, accumsan purus. 
-                                        </div>
+                                        
                                     </div>
                                 </div>
                                 
@@ -453,8 +414,7 @@
                     </div>
                 </section>
                 <!-- End Services Section -->
-                
-                
+
                 <!-- Call Action Section -->
                 <section class="page-section pt-0 pb-0 banner-section bg-dark" data-background="images/full-width-images/section-bg-2.jpg">
                     <div class="container relative">
@@ -465,13 +425,12 @@
                                 
                                 <div class="mt-140 mt-lg-80 mb-140 mb-lg-80">
                                     <div class="banner-content">
-                                        <h3 class="banner-heading font-alt">Looking for exclusive digital services?</h3>
+                                        <h3 class="banner-heading font-alt">Aumenta considerablemente tus posibilidades de trabajo</h3>
                                         <div class="banner-decription">
-                                            Proin fringilla augue at maximus vestibulum. Nam pulvinar vitae neque et porttitor. 
-                                            Integer non dapibus diam, ac eleifend lectus.
+                                        Con el creador de CV, puede crear rápida y fácilmente un CV distintivo y profesional en 15 minutos que se destaque.
                                         </div>
                                         <div class="local-scroll">
-                                            <a href="#contact" class="btn btn-mod btn-w btn-medium btn-round">Lets talk</a>
+                                            <a href="{{ route('generador.paso1.create') }}" class="btn btn-mod btn-w btn-medium btn-round">Crea tu CV</a>
                                         </div>
                                     </div>
                                 </div>
