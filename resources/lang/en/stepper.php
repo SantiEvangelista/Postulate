bb71850b9476b0d79ae1b9ed1e290dbf2086ddf6
JustAnotherProgramer98 <santievan98@gmail.com>
2025-03-22 01:45:19 -0300

    Textos para el step 1

---
+++ resources/lang/en/stepper.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    'steps' => [
+        'personal_info' => 'Personal Info',
+        'experience' => 'Experience',
+        'education' => 'Education'
+    ],
+    'personal_info' => [
+        'title' => 'Personal Information',
+        'first_name' => 'First Name',
+        'last_name' => 'Last Name',
+        'email' => 'Email',
+        'phone' => 'Phone',
+        'address' => 'Address',
+        'birth_date' => 'Date of Birth',
+        'profession' => 'Profession',
+        'about_me' => 'About Me',
+        'photo' => 'Photo'
+    ],
+    'buttons' => [
+        'next' => 'Next',
+        'back' => 'Back',
+        'save' => 'Save'
+    ],
+    'placeholders' => [
+        'first_name' => 'E.g: John',
+        'last_name' => 'E.g: Doe',
+        'email' => 'E.g: john.doe@email.com',
+        'phone' => 'E.g: +1 234 567-8900',
+        'address' => 'E.g: 123 Street, City',
+        'profession' => 'E.g: Web Developer',
+        'about_me' => 'Write a brief description about yourself...'
+    ]
+]; 
---
+++ resources/lang/es/stepper.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    'steps' => [
+        'personal_info' => 'Datos Personales',
+        'experience' => 'Experiencia',
+        'education' => 'Educación'
+    ],
+    'personal_info' => [
+        'title' => 'Información Personal',
+        'first_name' => 'Nombre',
+        'last_name' => 'Apellido',
+        'email' => 'Correo Electrónico',
+        'phone' => 'Teléfono',
+        'address' => 'Dirección',
+        'birth_date' => 'Fecha de Nacimiento',
+        'profession' => 'Profesión',
+        'about_me' => 'Sobre Mí',
+        'photo' => 'Foto'
+    ],
+    'buttons' => [
+        'next' => 'Siguiente',
+        'back' => 'Atrás',
+        'save' => 'Guardar'
+    ],
+    'placeholders' => [
+        'first_name' => 'Ej: Juan',
+        'last_name' => 'Ej: Pérez',
+        'email' => 'Ej: juan.perez@email.com',
+        'phone' => 'Ej: +54 9 11 1234-5678',
+        'address' => 'Ej: Calle 123, Ciudad',
+        'profession' => 'Ej: Desarrollador Web',
+        'about_me' => 'Escribe una breve descripción sobre ti...'
+    ]
+]; 
