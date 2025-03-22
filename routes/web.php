aff8183b92b91f022a88faf1a21b8c12ffb4d7c2
JustAnotherProgramer98 <santievan98@gmail.com>
2025-03-22 01:28:05 -0300

    Cambio de idioma + nueva ruta para nuevo front

--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,9 @@ Route::get('/', function () {return view('index');})->name('index');
 Route::get('/generatedPDF',[FillPDFController::class,'Addtopdf'])->name('resultado.pdf');;
 
 // Vistas GET
-Route::get('/generarCV/paso/1', [GeneradorController::class, 'create_paso1'])->name('generador.paso1.create');
-Route::get('/generarCV/paso/2', [GeneradorController::class, 'create_paso2'])->name('generador.paso2.create');
-Route::get('/generarCV/paso/3', [GeneradorController::class, 'create_paso3'])->name('generador.paso3.create');
+Route::get('/generarCV/paso/1', [GeneradorController::class, 'createPaso1'])->name('generador.paso1.create');
+Route::get('/generarCV/paso/2', [GeneradorController::class, 'createPaso2'])->name('generador.paso2.create');
+Route::get('/generarCV/paso/3', [GeneradorController::class, 'createPaso3'])->name('generador.paso3.create');
 
 Route::get('/generarCV/fin', [GeneradorController::class, 'success'])->name('generador.success');
 
@@ -31,3 +31,16 @@ Route::get('/generarCV/fin', [GeneradorController::class, 'success'])->name('gen
 Route::post('/generarCV/paso/1', [GeneradorController::class, 'post_paso1'])->name('generador.paso1.store');
 Route::post('/generarCV/paso/2', [GeneradorController::class, 'post_paso2'])->name('generador.paso2.store');
 Route::post('/generarCV/paso/3', [GeneradorController::class, 'post_paso3'])->name('generador.paso3.store');
+
+
+//nuevo front   
+Route::get('/nuevo-front', function () {
+    return view('home');
+});
+
+Route::get('language/{locale}', function ($locale) {
+    if (in_array($locale, ['en', 'es'])) {
+        session(['locale' => $locale]);
+    }
+    return redirect()->back();
+})->name('language.switch');
